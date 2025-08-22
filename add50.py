e81b13353b8b793be517e535dcd031b8dc5f0e60
buggs-bunny <94925603+buggs-bunny@users.noreply.github.com>
2025-08-22 14:01:16 +0530

    Add files via upload

---
+++ add50.py
@@ -0,0 +1,5 @@
+n=0
+for i in range(1,51):
+    n=n+i
+
+print("the sum of numbers from 1 to 50 is", n)
---
+++ oddeve.py
@@ -0,0 +1,6 @@
+n=float(input("Enter a number: "))
+a=n%2
+if a==0:
+    print(n,"is an even number")
+else:
+    print(n,"is an odd number")
